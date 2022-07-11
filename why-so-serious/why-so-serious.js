a12d11a413b9594058a34dddb358a361382cb10c
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2022-07-11 12:00:18 +0100

    Saving Kata Solution

---
+++ why-so-serious/why-so-serious.js
@@ -0,0 +1,5 @@
+function whySoSerious(str) {
+  if(str.indexOf("Jc")!=-1&&str.indexOf("7s")!=-1&&str.indexOf("As")!=-1&&str.indexOf("9c")!=-1&&str.indexOf("Jc")!=-1&&str.indexOf("5s")!=-1){
+  return true;}
+  return false;
+}
