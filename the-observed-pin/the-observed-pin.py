a9bb63a2f21039b68f1a5cfe46cd20b750c102dd
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2022-09-12 18:00:10 +0100

    Saving Kata Solution

---
+++ the-observed-pin/the-observed-pin.py
@@ -0,0 +1,6 @@
+import itertools
+dictio = {"1":["1","2","4"],"2":["1","2","3","5"],"3":["2","3","6"],"4":["1","4","5","7"],"5":["2","4","5","6","8"],"6":["3","5","6","9",],"7":["4","7","8"],"8":["5","7","8","9","0"],"9":["6","8","9"],"0":["0","8"]}
+def get_pins(observed):
+    lss = [dictio[i] for i in observed]
+    return  list(set(list("".join(i) for i in itertools.product(*lss))))
+    
