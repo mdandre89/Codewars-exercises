e98bc1099fe5eb6af85604008e9551d41fc6a6ef
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2023-01-11 17:00:01 +0000

    Saving Kata Solution

---
+++ simple-fun-number-152-invite-more-women/simple-fun-number-152-invite-more-women.js
@@ -0,0 +1,6 @@
+function inviteMoreWomen(L) {
+ 
+  var number = L.reduce(function(a,b){return a+b});
+  return number > 0 ? true: false;
+  
+}
---
+++ simple-fun-number-152-invite-more-women/simple-fun-number-152-invite-more-women.py
@@ -0,0 +1,2 @@
+def invite_more_women(arr):
+    return True if arr.count(1) > arr.count(-1) else False
