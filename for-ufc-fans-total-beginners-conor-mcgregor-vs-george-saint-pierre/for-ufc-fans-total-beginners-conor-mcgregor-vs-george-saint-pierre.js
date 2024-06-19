64f06f25deb1936d6ddb605826b8e2b2c56b2257
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2024-06-19 18:00:32 +0100

    Saving Kata Solution

---
+++ for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,7 @@
+var quote = function(fighter) {
+  if(fighter.toLowerCase()=="conor mcgregor"){
+    return  "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  }
+  else
+    return "I am not impressed by your performance.";
+};
