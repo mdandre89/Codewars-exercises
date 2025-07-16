e7360ce6bcd6ca75c2f07d5eb119219a41dad2b8
elephantcastle <obymik89@gmail.com>
2025-07-16 18:00:32 +0100

    Saving Kata Solution

---
+++ configure-package-json-for-a-node-application/configure-package-json-for-a-node-application.js
@@ -0,0 +1,7 @@
+const configuration = {
+  "name": "your-pack-name",
+  "version":"3.1.0",
+  "description": "fuck off"
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json
