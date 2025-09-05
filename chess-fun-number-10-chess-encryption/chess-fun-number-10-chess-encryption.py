d0c84d642d223146ba417982954e733c6e054235
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2025-09-05 16:00:01 +0100

    Saving Kata Solution

---
+++ chess-fun-number-10-chess-encryption/chess-fun-number-10-chess-encryption.py
@@ -0,0 +1,3 @@
+dictio = {"a":"d3","b":"d4","c":"d5","d":"d6","e":"d7","f":"e7","g":"e6","h":"e5","i":"e4","j":"e3","k":"e2","l":"f2","m":"g2","n":"h2","o":"h1","p":"g1","q":"f1","r":"e1","s":"d1","t":"c1","u":"b1","v":"a1","w":"a2","x":"b2","y":"c2","z":"d2"}
+def chess_encryption(msg):
+    return msg.translate(str.maketrans(dictio))
