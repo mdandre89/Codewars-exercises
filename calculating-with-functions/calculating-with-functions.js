80d7eff1b7717a2ced1aa97ca54344a538b1098e
hadeszagros <42893607+mdandre89@users.noreply.github.com>
2025-09-22 19:00:01 +0100

    Saving Kata Solution

---
+++ calculating-with-functions/calculating-with-functions.js
@@ -0,0 +1,45 @@
+function zero() {
+  if (arguments.length > 0) return eval(0 + arguments[0]);
+  else return 0;
+}
+function one() {
+  if (arguments.length > 0) return eval(1 + arguments[0]);
+  else return 1;
+}
+function two() {
+  if (arguments.length > 0) return eval(2 + arguments[0]);
+  else return 2;
+}
+function three() {
+  if (arguments.length > 0) return eval(3 + arguments[0]);
+  else return 3;
+}
+function four() {
+  if (arguments.length > 0) return eval(4 + arguments[0]);
+  else return 4;
+}
+function five() {
+  if (arguments.length > 0) return eval(5 + arguments[0]);
+  else return 5;
+}
+function six() {
+  if (arguments.length > 0) return eval(6 + arguments[0]);
+  else return 6;
+}
+function seven() {
+  if (arguments.length > 0) return eval(7 + arguments[0]);
+  else return 7;
+}
+function eight() {
+  if (arguments.length > 0) return eval(8 + arguments[0]);
+  else return 8;
+}
+function nine() {
+  if (arguments.length > 0) return eval(9 + arguments[0]);
+  else return 9;
+}
+
+function plus() {return "+" + arguments[0];}
+function minus() {return "-" + arguments[0];}
+function times() {return "*" + arguments[0];}
+function dividedBy() {return "/" + arguments[0];}
